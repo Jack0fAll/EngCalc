a634f537d0ca10228f1663792d10da70a8a37a08
Jack0fAll <j4ck0f4ll@gmail.com>
2018-08-02 14:33:16 -0400

    Added ability to add new element to history

--- web/main.dart
+++ web/main.dart
@@ -2,4 +2,12 @@ import 'dart:html';
 
 void main() {
   querySelector('#initial-output').remove(); //Show a loading message until this script loads
+  addToHistory();
+}
+
+void addToHistory() {
+  Element historyList = querySelector('.history ul');
+  Element newListItem = LIElement();
+  newListItem.text = "I'm Added from dart";
+  historyList.children.add(newListItem);
 }
